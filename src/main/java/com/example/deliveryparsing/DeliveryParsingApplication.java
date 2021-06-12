583b94a882ff1e7dfb49b9f6a131f5873aac2595
daniel zheng <daniel.zheng@evolutionq.com>
2021-06-12 14:56:29 -0400

    initial commit

---
+++ src/main/java/com/example/deliveryparsing/DeliveryParsingApplication.java
@@ -0,0 +1,13 @@
+package com.example.deliveryparsing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryParsingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DeliveryParsingApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/deliveryparsing/DeliveryParsingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deliveryparsing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryParsingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
